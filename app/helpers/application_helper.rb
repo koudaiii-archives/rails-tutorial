e26c910a5ae4d00e8913854bac98ec926cebbd51
koudaiii <cs006061@gmail.com>
2015-05-18 09:34:28 +0900

    fix test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
-    elsif
+    else
       "#{base_title} | #{page_title}"
     end
   end
