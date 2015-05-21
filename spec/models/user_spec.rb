cf7000a88c771d0359820a3ab0bfbbaec54dccba
koudaiii <cs006061@gmail.com>
2015-05-22 00:21:33 +0900

    add max lenght user name and spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,10 @@ describe User do
   it { should respond_to(:email) }
 
   it { should be_valid }
-
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
