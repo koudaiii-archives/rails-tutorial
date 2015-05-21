67fd8f0d74e7b20f1d66889570efb0b12339f845
koudaiii <cs006061@gmail.com>
2015-05-21 11:11:40 +0900

    fix typo

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe "UserPages", type: :request do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
-    it { should nave_title(full_title('Sign up')) }
+    it { should have_title(full_title('Sign up')) }
   end
 end
