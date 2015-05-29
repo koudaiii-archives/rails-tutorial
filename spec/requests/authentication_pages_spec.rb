988b6e4faab52acab23224d1ee7408a2993d95b0
koudaiii <cs006061@gmail.com>
2015-05-29 18:06:07 +0900

    add form in signin

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -3,12 +3,32 @@ require 'rails_helper'
 RSpec.describe "AuthenticationPages", type: :request do
   describe "Authentication" do
     subject { page }
+    before { visit signin_path }
 
     describe "signin page" do
-      before { visit signin_path }
 
       it { should have_content('Sign in') }
       it { should have_title('Sign in') }
+    end
+
+    describe "invalid signin" do
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+
+    describe "valid signin" do
+      let(:user){ FactoryGirl.create(:user) }
+      before do
+        fill_in "Email", with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link("Profile", href: user_path(user)) }
+      it { should have_link("Sign out", href: signout_path(user)) }
+      it { should_not have_link('Sign in', href: signin_path) }
     end
   end
 end
