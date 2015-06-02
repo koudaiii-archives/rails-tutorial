cf0b19dca0ae31ee72e38bb206ffbc5439581419
koudaiii <cs006061@gmail.com>
2015-06-02 09:23:26 +0900

    add function sign_out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,8 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destory
+  def destroy
+    sign_out
+    redirect_to root_url
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,18 +10,18 @@ RSpec.describe "AuthenticationPages", type: :request do
         it { should have_content('Sign in') }
         it { should have_title('Sign in') }
       end
-  
+
       describe "with invalid signin" do
         before { click_button "Sign in" }
         it { should have_title('Sign in') }
         it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-  
+
         describe "after visiting another page" do
           before { click_link "Home" }
           it { should_not have_selector('div.alert.alert-error') }
         end
       end
-  
+
       describe "with valid signin" do
         let(:user){ FactoryGirl.create(:user) }
         before do
@@ -29,11 +29,15 @@ RSpec.describe "AuthenticationPages", type: :request do
           fill_in "Password", with: user.password
           click_button "Sign in"
         end
-  
+
         it { should have_title(user.name) }
         it { should have_link("Profile", href: user_path(user)) }
         it { should have_link("Sign out", href: signout_path) }
         it { should_not have_link('Sign in', href: signin_path) }
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
       end
     end
   end
