c06a843721cd9d9aa774c1e48692e8811f273885
koudaiii <cs006061@gmail.com>
2015-06-16 10:12:58 +0900

    add layout in followers and followings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,    only: [:edit, :update]
   before_action :admin_user, only: :destroy
   before_action :non_user, only: [:new, :create]
@@ -47,25 +48,39 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
-  #Brefore actions
+    #Brefore actions
 
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
-  end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
-  def admin_user
-    redirect_to(root_path) unless current_user.admin?
-  end
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 
-  def non_user
-    redirect_to root_path if signed_in?
-  end
+    def non_user
+      redirect_to root_path if signed_in?
+    end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -50,56 +50,66 @@ RSpec.describe "AuthenticationPages", type: :request do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
         end
-      end
-      describe "as wrong user" do
-        let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-        before { sign_in user, no_capybara: true }
+        describe "as wrong user" do
+          let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+          before { sign_in user, no_capybara: true }
 
-        describe "submitting a GET request to the Users#edit action" do
-          before { get edit_user_path(wrong_user) }
-          specify { expect(response.body).not_to match(full_title('Edit user')) }
-          specify { expect(response).to redirect_to(root_url) }
-        end
-
-        describe "submitting a PATCH request to the Users#update action" do
-          before { patch user_path(wrong_user) }
-          specify { expect(response).to redirect_to(root_path) }
-        end
-      end
-      describe "for non-signed-in users" do
-        it { should_not have_link('Users',    href: users_path) }
-        it { should_not have_link('Profile',  href: user_path(user)) }
-        it { should_not have_link('Settings', href: edit_user_path(user)) }
-        it { should_not have_link('Sign out', href: signout_path) }
-        it { should have_link('Sign in', href: signin_path) }
-
-        describe "when attempting to visit a protected page" do
-          before do
-            visit edit_user_path(user)
-            sign_in user
+          describe "submitting a GET request to the Users#edit action" do
+            before { get edit_user_path(wrong_user) }
+            specify { expect(response.body).not_to match(full_title('Edit user')) }
+            specify { expect(response).to redirect_to(root_url) }
           end
 
-          describe "after signing in" do
-            it "should render the desired protected page" do
-              expect(page).to have_title('Edit user')
+          describe "submitting a PATCH request to the Users#update action" do
+            before { patch user_path(wrong_user) }
+            specify { expect(response).to redirect_to(root_path) }
+          end
+        end
+        describe "for non-signed-in users" do
+          it { should_not have_link('Users',    href: users_path) }
+          it { should_not have_link('Profile',  href: user_path(user)) }
+          it { should_not have_link('Settings', href: edit_user_path(user)) }
+          it { should_not have_link('Sign out', href: signout_path) }
+          it { should have_link('Sign in', href: signin_path) }
+
+          describe "when attempting to visit a protected page" do
+            before do
+              visit edit_user_path(user)
+              sign_in user
             end
 
-            describe "when signing in again" do
-              before do
-                delete signout_path
-                sign_in user
+            describe "after signing in" do
+              it "should render the desired protected page" do
+                expect(page).to have_title('Edit user')
               end
 
-              it "should render the default(profile) page" do
-                expect(page).to have_title(user.name)
+              describe "when signing in again" do
+                before do
+                  delete signout_path
+                  sign_in user
+                end
+
+                it "should render the default(profile) page" do
+                  expect(page).to have_title(user.name)
+                end
               end
             end
           end
-        end
 
-        describe "visiting the user index" do
-          before { visit users_path }
-          it { should have_title('Sign in') }
+          describe "visiting the user index" do
+            before { visit users_path }
+            it { should have_title('Sign in') }
+          end
+
+          describe "visiting the following page" do
+            before { visit following_user_path(user) }
+            it { should have_title('Sign in') }
+          end
+
+          describe "visiting the followers page" do
+            before { visit followers_user_path(user) }
+            it { should have_title('Sign in') }
+          end
         end
 
         describe "in the Microposts controller" do
