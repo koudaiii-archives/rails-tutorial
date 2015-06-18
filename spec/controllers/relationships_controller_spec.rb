b33bea3479815875b3da26c1ad9b89f8d2939762
koudaiii <cs006061@gmail.com>
2015-06-18 20:22:11 +0900

    add Ajax for follow and unfollow

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe "RelationshipsController", type: :request do
 
     it "should incremment the Relationship count" do
       expect do
-        xhr :post, :create, relationship: { followed_id: other_user.id }
+        xhr :post, relationships_path, relationship: { followed_id: other_user.id }
       end.to change(Relationship, :count).by(1)
     end
 
     it "should respond with success" do
-      xhr :post, :create, relationship: { followed_id: other_user.id }
+      xhr :post, relationships_path, relationship: { followed_id: other_user.id }
       expect(response).to be_success
     end
   end
@@ -23,18 +23,18 @@ RSpec.describe "RelationshipsController", type: :request do
   describe "destroying a relationship with Ajax" do
 
     before { user.follow!(other_user) }
-    let(:retaionship) do
+    let(:relationship) do
       user.relationships.find_by(followed_id: other_user.id)
     end
 
     it "should decrement the Relationship count" do
       expect do
-        xhr :delete, :destroy, id: relationship.id
-      end.to change(Relationship :count).by(-1)
+        xhr :delete, relationship_path(relationship.id), id: relationship.id
+      end.to change(Relationship, :count).by(-1)
     end
 
     it "should respond with success" do
-      xhr :delete, :destroy, id: relationship.id
+      xhr :delete, relationship_path(relationship.id), id: relationship.id
       expect(response).to be_success
     end
   end
