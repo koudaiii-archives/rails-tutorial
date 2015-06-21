90dc9b81946e6447d17e9a83a6344bb5e84b870c
koudaiii <cs006061@gmail.com>
2015-06-21 16:42:37 +0900

    add column in users for reply to micropost

---
+++ db/migrate/20150621073515_user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Migration
+  def change
+    add_column :users, :account_name, :string
+    add_index  :users, :account_name, :unique => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150614123503) do
+ActiveRecord::Schema.define(version: 20150621073515) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
@@ -39,8 +39,10 @@ ActiveRecord::Schema.define(version: 20150614123503) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           default: false
+    t.string   "account_name"
   end
 
+  add_index "users", ["account_name"], name: "index_users_on_account_name", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
