f0eb65c164f3ca353c6ab87401acc50217be2553
koudaiii <cs006061@gmail.com>
2015-06-24 15:58:29 +0900

    add schema in messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622033955) do
+ActiveRecord::Schema.define(version: 20150624040306) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.string   "content"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
