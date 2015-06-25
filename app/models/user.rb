569a3278939163522aa72308b8408f4a423c11aa
koudaiii <cs006061@gmail.com>
2015-06-25 14:05:21 +0900

    fix relation in reply to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,10 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
 
   has_many :microposts, dependent: :destroy
-  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reply_to, through: :microposts, source: :in_reply_to
   has_many :followed_users, through: :relationships, source: :followed
 
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_relationships, foreign_key: "followed_id",
                                   class_name: "Relationship",
                                   dependent: :destroy
