b4ff519915dc3a359d66b91cb0c7f913b899d44a
koudaiii <cs006061@gmail.com>
2015-06-26 12:41:43 +0900

    set belongs_to for sender

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  belongs_to :Users
+  belongs_to :sender, class_name: "User"
   validates :sender_id, presence: true
   validates :recipient_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
