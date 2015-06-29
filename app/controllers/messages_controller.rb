8d9feb638e6c36d33219cf2b8c701a17d9373653
koudaiii <cs006061@gmail.com>
2015-06-29 20:33:21 +0900

    add index in Direct Messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
-    @messages = current_user.messages
+    @messages = current_user.messages.paginate(page: params[:page])
   end
 
   # GET /messages/1
