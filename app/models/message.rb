dbefc309f4fe706852122ac3084d20c26a1354ef
koudaiii <cs006061@gmail.com>
2015-07-06 16:31:14 +0900

    add default_scop

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: "User"
   belongs_to :recipient, class_name: "User"
 
+  default_scope -> { order('created_at DESC') }
+
   validates :sender_id, presence: true
   validates :recipient_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
