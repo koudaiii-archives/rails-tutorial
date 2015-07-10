1fb5d16b72578e8758bcf570da697990a61a1348
koudaiii <cs006061@gmail.com>
2015-07-10 11:45:29 +0900

    fix controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,4 @@
+# Notification
 class UserMailer < ApplicationMailer
   default from: 'notification@example.com'
 
@@ -5,6 +6,11 @@ class UserMailer < ApplicationMailer
     @user = user
     email_with_name = %("#{user.name}" <#{user.email}>)
     @url  = 'http://localhost:3000'
-    mail(to: email_with_name, subject: 'Welcom to My  Awesome Site')
+
+    mail(to: email_with_name,
+         subject: 'Welcom to My  Awesome Site') do |format|
+           format.html { render 'followed' }
+           format.text { render text: 'followed' }
+         end
   end
 end
