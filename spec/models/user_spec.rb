1fabb91107257fa4393bd93aa41816b4387fac1e
koudaiii <cs006061@gmail.com>
2015-07-16 01:21:20 +0900

    add spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,7 @@ describe User do
   it { should respond_to(:following?) }
   it { should respond_to(:follow!) }
   it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:receive_notifications_by_email) }
   it { should respond_to(:followers) }
   it { should be_valid }
   it { should_not be_admin }
