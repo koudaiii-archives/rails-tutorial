e3a6df3749b46f3ff24eaf08f31592cd8d52cff5
koudaiii <cs006061@gmail.com>
2015-07-27 17:05:59 +0900

    add outline

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,14 @@
 class PasswordResetsController < ApplicationController
+
   def new
+  end
+
+  def create
+    unless params[:email]
+      flash.alert = "Oooops! Sorry!"
+      redirect_to root_path
+    end
+    flash.notice = "Ok! send to your Email!"
+    redirect_to root_path
   end
 end
--- spec/views/password_resets/new.html.erb_spec.rb
+++ spec/views/password_resets/new.html.erb_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "password_resets/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { page }
+  describe "new password reset page" do
+    before { visit new_password_reset_path }
+    it {  should have_content("Forgotten Password?") }
+
+  end
 end
