80b2481b65ef45049ad5b2d1ad09a15224f8771d
koudaiii <cs006061@gmail.com>
2015-07-28 01:10:42 +0900

    format email and not present

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,14 +1,22 @@
 class PasswordResetsController < ApplicationController
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
+  before_filter :set_params, only: :create
+  before_filter :format_to?, only: :create
   def new
   end
 
   def create
-    unless params[:email]
-      flash.alert = "Oooops! Sorry!"
-      redirect_to root_path
-    end
     flash.notice = "Ok! send to your Email!"
     redirect_to root_path
   end
+
+  private
+    def set_params
+      return redirect_to new_password_reset_path, notice: "Please set your Email" if params[:email].blank?
+    end
+
+    def format_to?
+      return redirect_to new_password_reset_path, notice: "Sorry!not email format" unless params[:email] =~ VALID_EMAIL_REGEX 
+    end
 end
--- spec/views/password_resets/new.html.erb_spec.rb
+++ spec/views/password_resets/new.html.erb_spec.rb
@@ -4,7 +4,20 @@ RSpec.describe "password_resets/new.html.erb", type: :view do
   subject { page }
   describe "new password reset page" do
     before { visit new_password_reset_path }
+
     it {  should have_content("Forgotten Password?") }
+
+    describe "not present" do
+      before { click_button "Reset Password" }
+      it { should have_content("Please set your Email") }
+    end
+    describe "format email" do
+      before do
+        fill_in "Email", with: "test"
+        click_button "Reset Password"
+      end
+      it { should have_content("Sorry!not email format") }
+    end
 
   end
 end
