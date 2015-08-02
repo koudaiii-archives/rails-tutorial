d04d2fa0abb8fea81162b677f93403aab235ceac
koudaiii <cs006061@gmail.com>
2015-08-02 23:32:29 +0900

    add func reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,7 +16,23 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(password_reset_token: params[:id])
   end
 
+  def update
+    @user = User.find_by(password_reset_token: params[:id])
+    if  @user.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path, :alert => "Password reset has expired."
+    elsif  @user.update_attributes(user_params)
+      redirect_to root_url, :notice => "Password has been reset!"
+    else
+      render :edit
+    end
+  end
+
   private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
     def set_params
       return redirect_to new_password_reset_path, notice: "Please set your Email" if params[:email].blank?
     end
