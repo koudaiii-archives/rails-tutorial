a045f4aec37ac70b70d0e7a7e43f602d4ee667f2
koudaiii <cs006061@gmail.com>
2015-08-04 23:49:40 +0900

    add column state to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722071818) do
+ActiveRecord::Schema.define(version: 20150804144351) do
 
   create_table "messages", force: :cascade do |t|
     t.integer  "sender_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150722071818) do
     t.boolean  "receive_notifications_by_email"
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
+    t.string   "state"
   end
 
   add_index "users", ["account_name"], name: "index_users_on_account_name", unique: true
